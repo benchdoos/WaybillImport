987c0baed779e9249ad9f9fe10e3dced03a56563
Eugene zrazhevsky <eugeny.zrazhevsky@gmail.com>
2017-10-09 14:00:08 +0300

    //Once I've read that you should not practice storing application constants at interfaces. So... I won't

--- src/main/java/com/maykop_mmz/ppo/waybillImport/core/ApplicationConstants.java
+++ src/main/java/com/maykop_mmz/ppo/waybillImport/core/ApplicationConstants.java
@@ -2,12 +2,12 @@ package com.maykop_mmz.ppo.waybillImport.core;
 
 import java.io.File;
 
-public interface ApplicationConstants {
-    String APP_NAME = "WaybillImport";
-    String APPLICATION_TMP_FOLDER = System.getProperty("java.io.tmpdir")
+public class ApplicationConstants {
+    public static final String APP_NAME = "WaybillImport";
+    public static final String APPLICATION_TMP_FOLDER = System.getProperty("java.io.tmpdir")
             + APP_NAME + File.separator;
-    String APP_LOG_FOLDER_PATH = APPLICATION_TMP_FOLDER+ "logs";
-    String APP_LOG_PROPERTY = "WaybillImport.log.folder";
-    String PROPERTIES_FILE_PATH = APPLICATION_TMP_FOLDER + "settings.xml";
-    String APP_BACKUP_FOLDER = System.getProperty("user.home") + File.separator + APP_NAME + File.separator + "backup";
+    public static final String APP_LOG_FOLDER_PATH = APPLICATION_TMP_FOLDER+ "logs";
+    public static final String APP_LOG_PROPERTY = "WaybillImport.log.folder";
+    public static final String PROPERTIES_FILE_PATH = APPLICATION_TMP_FOLDER + "settings.xml";
+    public static final String APP_BACKUP_FOLDER = System.getProperty("user.home") + File.separator + APP_NAME + File.separator + "backup";
 }
